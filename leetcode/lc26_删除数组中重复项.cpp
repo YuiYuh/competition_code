b93639869312c8ea1a912397aff61ea82cc0d787
YuiYuh <270488307@qq.com>
2025-11-29 10:42:23 +0800

    new

---
+++ leetcode/lc26_删除数组中重复项.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+class Solution{
+public:
+	int removeDuplicates(vector<int>& nums){
+		int slow = 0;
+		for(int fast = 1; fast < nums.size(); fast++){
+			if(nums[fast] != nums[slow]){
+				nums[++slow] = nums[fast];
+			}
+		}
+		return slow+1;
+	}
+};
+
+//[1,2,2,3,4,5]
+//[1,2,3,4,5,?]
