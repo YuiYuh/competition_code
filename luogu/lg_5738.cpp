105ebf8eb75ecd6f9c84f2859009fdb2f9a8bf45
Yui_mac <pingguo@37MacBook-Air.local>
2025-12-12 23:23:23 +0800

    20251212

--- luogu/lg_5738.cpp
+++ luogu/lg_5738.cpp
@@ -1,29 +1,30 @@
 #include <iostream>
 #include <vector>
-#include<iomanip>
-#include<iterator>
+#include <iomanip>
+#include <iterator>
+#include <algorithm>
 using namespace std;
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int n,m;
     cin >> n >> m;
-    vector<int> v1;
     vector<double> result;
-    while(n--){
-        while(m--){
-            int tem;
-            int sum = 0;
-            double res = 0;
-            cin >> tem;
-            v1.push_back(tem);
-            for(auto p = next(v1.begin()); p != prev(v1.end()); p++){
-                sum += *p;
-            }
-            res = double(sum) / (m - 2);
-            result.push_back(res);
+    for(int i = 0; i < n; i++){
+        vector<int> v(m);
+        for(int j = 0; j < m; j++){
+            cin >> v[j];
         }
+            sort(v.begin(),v.end());
+            int sum = 0;
+            for(int j = 1; j < m - 1; j++){
+                sum += v[j];
+            }
+            double res = 1.0 * sum / (m - 2);
+            result.push_back(res);
+        
     }
-    cout << fixed << setprecision(2) << *(result.end()-1) << '\n';
+    sort(result.begin(),result.end());
+    cout << fixed << setprecision(2) << result.back() << '\n';
     return 0;
 }
