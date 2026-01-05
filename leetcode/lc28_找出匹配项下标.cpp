07494ba705f7a30fbb943b7ada760fe432eaa6fd
YuiYuh <270488307@qq.com>
2026-01-05 23:04:17 +0800

    20260105

--- leetcode/lc28_找出匹配项下标.cpp
+++ leetcode/lc28_找出匹配项下标.cpp
@@ -3,7 +3,12 @@ using namespace std;
 
 class Solution {
 public:
+    void getNext(int* next, string &s){
+        int j = -1;
+        next[0] = j;
+        
+    }
     int strStr(string haystack, string needle) {
-
+        
     }
 };
