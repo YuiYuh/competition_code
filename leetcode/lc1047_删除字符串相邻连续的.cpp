7141e101e608e0d65908f002e57a1a77a1d56ffa
YuiYuh <270488307@qq.com>
2026-01-21 22:06:54 +0800

    20260121

---
+++ leetcode/lc1047_删除字符串相邻连续的.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<char> st;
+        for(int i = 0; i < s.size(); i++){
+            if(st.empty() || st.top() != s[i]){
+                st.push(s[i]);
+            }else{
+                st.pop();
+            }
+        }
+        string res = "";
+        while(!st.empty()){
+            res += st.top();
+            st.pop();
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
