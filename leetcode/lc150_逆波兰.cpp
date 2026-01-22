d8a980a3bfe6264650794e1f37ed8f210153bb42
YuiYuh <270488307@qq.com>
2026-01-22 22:43:53 +0800

    20260122

---
+++ leetcode/lc150_逆波兰.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<long long> st;
+        for(int i = 0; i < tokens.size(); i++){
+            if(tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/"){
+                long long num1 = st.top();
+                st.pop();
+                long long num2 = st.top();
+                st.pop();
+               if(tokens[i] == "+") st.push(num1 + num2); 
+               if(tokens[i] == "-") st.push(num2 - num1);
+               if(tokens[i] == "*") st.push(num1 * num2);
+               if(tokens[i] == "/") st.push(num2 / num1);
+            }else{
+                st.push(stoll(tokens[i]));
+            }
+        }
+        long long result = st.top();
+        st.pop();
+        return result;
+    }
+};
