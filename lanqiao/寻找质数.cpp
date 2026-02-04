268106b6b32b990227696c3f9bbecfd95b8ce408
YuiYuh <270488307@qq.com>
2026-02-04 22:12:43 +0800

    20260204

---
+++ lanqiao/寻找质数.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//bool isPrime(int n){
+//	if(n < 2){
+//		return false;
+//	}
+//	if(n == 2) return true;
+//	if(n % 2 == 0) return false;
+//	for(int i = 3; i * i <= n; i += 2){
+//		if(n % i == 0) return false;
+//	}
+//} 
+//int main(){
+//	int cnt = 0;
+//	for(int i = 2;;i++){
+//		if(isPrime(i)){
+//			cnt++;
+//		}
+//		if(cnt == 2025){
+//			cout << i << '\n';
+//			break;
+//		}
+//	}
+//	return 0;
+//}
+
+
+int main(){
+	int v[5][5];
+	
+	return 0;
+} 
+
+
+
+
+
+
+
+
