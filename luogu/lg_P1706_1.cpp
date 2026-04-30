829e42bf449ba848cf6c69078f55b74a3bcefcc8
YuiYuh <270488307@qq.com>
2026-04-30 10:17:43 +0800

    20260430

---
+++ luogu/lg_P1706_1.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+const int MAXN = 10;
+int arr[MAXN];
+bool vis[MAXN];
+int n;
+
+void dfs(int x){
+    if(x > n){
+        for(int i = 1; i <= n; ++ i){
+            cout << setw(5) << arr[i];
+        }
+        cout << '\n';
+        return ;
+    }
+    for(int i = 1; i <= n; ++ i){
+        if(!vis[i]){
+            vis[i] = true;
+            arr[x] = i;
+            dfs(x + 1);
+            vis[i] = false;
+        }
+    }
+}
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0),cout.tie(0);
+
+    cin >> n;
+    dfs(1);
+
+    return 0;
+}
