bb5b9dc79b47ab2cfbeec1deb8e78f9481a66d19
YuiYuh <270488307@qq.com>
2026-05-27 17:14:04 +0800

    20260527

---
+++ luogu/lg_P1939.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+
+const int MOD = 1e9 + 7;
+const int MAXN = 2e9 + 10;
+int arr[MAXN];
+int T;
+
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0),cout.tie(0);
+
+    cin >> T;
+    while (T--)
+    {
+        int n;
+        cin >> n;
+
+    }
+    
+
+
+
+
+
+    return 0;
+}
